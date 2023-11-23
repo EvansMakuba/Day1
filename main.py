5f64923aed1294a22210016db0c573e5c0d5ca37
EvansMakuba <60476820+EvansMakuba@users.noreply.github.com>
2023-11-23 04:25:43 -0800

    day one commit
    A seemingly useless program

---
+++ main.py
@@ -0,0 +1,10 @@
+# 1. Create a greeting for your program
+print("Hello and welcome to Brand Name Generator program")
+# 2. Ask the user for the city they grew up in
+city = input("What is the name of the city you were born?\n")
+# 3. Ask the user for the name of a pet
+pet = input("What is the name of your pet?\n")
+# 4. Combine the name of the city and the pet to show their brand name
+brand_name = city+pet
+print(f"{brand_name}")
+# 5. Make sure the input cursor show up on a new line
